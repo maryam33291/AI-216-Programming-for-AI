b040364cab767ed00526b49c37594a66fab9fa83
Maryam Khalid <khalidmaryam432@gmail.com>
2026-02-08 20:39:36 +0500

    Lab02: add course repo structure + week02 hello script

---
+++ lab no 2/week02/hello_ai216.py
@@ -0,0 +1,11 @@
+import sys
+
+print("Name: Maryam")
+print("10039")
+
+print("Python Version:")
+print(sys.version)
+
+print("Programming for AI means learning how to")
+print("use programming to build intelligent systems")
+print("that can solve real world problems.")
